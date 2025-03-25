c8cb04c71d9e901bfd6c7f955e460776dfbcd69d
Camilly Lazzaro <camilly.l@aluno.ifsp.edu.br>
2025-03-25 07:18:49 -0300

    feat: add Album component

--- ETAPA1/react-exemplos/src/components/Photo.jsx
+++ ETAPA1/react-exemplos/src/components/Photo.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const Photo = ({photo}) => {
-  return (
-    <>
-      <img src={photo.thumbnailUrl} alt={photo.title} />
-      <h2>ID #{photo.id} {photo.title}</h2>
-    </>
-  );
-}
-
+const Photo = ( {photo}) => {
+    return (
+        <>
+            <img src={photo.thumbnailUrl} alt={photo.title} />
+            <h2>ID #{photo.id} {photo.title}</h2>
+        </>
+    );
+};
 export default Photo;
