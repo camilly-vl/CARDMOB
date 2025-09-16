913826ee3b346fbef3eab799b5e79f806dc8c7dc
CAMILLY VITORIA LAZZARO FERREIRA <CG3026311@car.ifsp>
2025-09-16 09:01:17 -0300

    Suporte ao arquivo .env

--- ETAPA3/loja-react-app/src/services/authService.ts
+++ ETAPA3/loja-react-app/src/services/authService.ts
@@ -1,15 +1,17 @@
-import Config from "react-native-config";
+import Constants from 'expo-constants';
 
-const API_URL = 'http://10.81.205.31:5000';
+// const API_URL = Configs.API_URL;
+// const API_URL = 'http://10.81.205.29:5000';
+const { apiUrl } = Constants.expoConfig?.extra || {};
 
 export async function fakeLogin(email: string, password: string): Promise<string> {
-    console.log(API_URL);
+    console.log(apiUrl);
     if (email === 'teste@example.com' && password === '123') {
         return Promise.resolve('fake-jwt-token');
     }
     if (email === 'admin@example.com' && password === 'admin123') {
         try {
-            const response = await fetch(`${API_URL}/api/users/login`, {
+            const response = await fetch(`${apiUrl}/api/users/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -33,9 +35,9 @@ export async function requestLogin(
     email: string,
     password: string
 ): Promise<string> {
-    console.log(API_URL);
+    console.log(apiUrl);
         try {
-            const response = await fetch(`${API_URL}/api/users/login`, {
+            const response = await fetch(`${apiUrl}/api/users/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
--- ETAPA3/loja-react-app/src/services/catalogServices.ts
+++ ETAPA3/loja-react-app/src/services/catalogServices.ts
@@ -1,16 +1,17 @@
-const API_URL = 'http://10.81.205.33:5000';
+import Constants from 'expo-constants';
 
+const { apiUrl } = Constants.expoConfig?.extra || {};
 
-export async function getCatalog(): Promise<any[]> { // alterado
+export async function getCatalog(): Promise<any[]> {
     try {
-        const response = await fetch (`${API_URL}/api/catalog`);
+        const response = await fetch(`${apiUrl}/api/catalog`);
         const data = await response.json();
         // console.log(data);
         // return Promise.resolve(data.catalog);
-        return data.catalog; // incluido/alterado
+        return data.catalog;
     }
     catch (error) {
-            console.error(error);
-            return Promise.reject('Erro ao obter produtos');
-        }
+        console.error(error);
+        return Promise.reject('Erro ao obter produtos');
+    }
 }
